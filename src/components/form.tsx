15b290392a2d7752ea12a49f7e9568b156b2e236
garryismael <tahinjanaharygarry@gmail.com>
2022-09-03 16:11:33 +0300

    update

--- src/components/form.tsx
+++ src/components/form.tsx
@@ -36,13 +36,13 @@ const Form: FC<FuncProps> = ({ onSubmit, loading }) => {
               className='block mb-2 text-xs font-bold tracking-wide text-gray-700 uppercase'
               htmlFor='galonX'
             >
-              Gallon 1
+              Carafe 1
             </label>
             <input
               className='block w-full px-4 py-3 mb-3 leading-tight text-gray-700 bg-gray-200 border rounded appearance-none focus:outline-none focus:bg-white'
               id='galonX'
               type='number'
-              placeholder='Galon X'
+              placeholder='Carafe X'
               value={galonX}
               onChange={setGalonXEvent}
               min='1'
@@ -53,13 +53,13 @@ const Form: FC<FuncProps> = ({ onSubmit, loading }) => {
               className='block mb-2 text-xs font-bold tracking-wide text-gray-700 uppercase'
               htmlFor='galonY'
             >
-              Gallon 2
+              Carafe 2
             </label>
             <input
               className='block w-full px-4 py-3 leading-tight text-gray-700 bg-gray-200 border border-gray-200 rounded appearance-none focus:outline-none focus:bg-white focus:border-gray-500'
               id='galonY'
               type='number'
-              placeholder='Galon Y'
+              placeholder='Carafe Y'
               value={galonY}
               onChange={setGalonYEvent}
               min='1'
