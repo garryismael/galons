50b1737d9c5b5d64d5ea7a3f19eb4f8f2109bfe2
garryismael <tahinjanaharygarry@gmail.com>
2022-09-03 15:47:51 +0300

    update form

--- src/components/form.tsx
+++ src/components/form.tsx
@@ -36,7 +36,7 @@ const Form: FC<FuncProps> = ({ onSubmit, loading }) => {
               className='block mb-2 text-xs font-bold tracking-wide text-gray-700 uppercase'
               htmlFor='galonX'
             >
-              Galon 1
+              Gallon 1
             </label>
             <input
               className='block w-full px-4 py-3 mb-3 leading-tight text-gray-700 bg-gray-200 border rounded appearance-none focus:outline-none focus:bg-white'
@@ -53,7 +53,7 @@ const Form: FC<FuncProps> = ({ onSubmit, loading }) => {
               className='block mb-2 text-xs font-bold tracking-wide text-gray-700 uppercase'
               htmlFor='galonY'
             >
-              Galon 2
+              Gallon 2
             </label>
             <input
               className='block w-full px-4 py-3 leading-tight text-gray-700 bg-gray-200 border border-gray-200 rounded appearance-none focus:outline-none focus:bg-white focus:border-gray-500'
@@ -86,7 +86,7 @@ const Form: FC<FuncProps> = ({ onSubmit, loading }) => {
             <span className='w-full px-3 mb-6 md:w-1/4 md:mb-0'></span>
             <button
               type='submit'
-              className='block w-full px-4 py-3 mb-3 leading-tight text-white button text-sm not-italic font-bold'
+              className='block w-full px-4 py-3 mb-3 text-sm not-italic font-bold leading-tight text-white button'
               disabled={loading}
             >
               <svg
@@ -107,7 +107,7 @@ const Form: FC<FuncProps> = ({ onSubmit, loading }) => {
                   fill='currentColor'
                 />
               </svg>
-              Proceder
+              Procéder
             </button>
           </div>
         </div>
